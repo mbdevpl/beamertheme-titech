df9a7022a3d929fe72aef7698735423765e8d755
Mateusz Bysiek <mateusz.bysiek@gmail.com>
2019-07-22 08:21:04 +0900

    change extractor script so that warning is more visible

--- extract_ppt_images.sh
+++ extract_ppt_images.sh
@@ -5,15 +5,16 @@ PPT_EN_MD5=95b6edf68e225f73a6055a55497dc270
 TEMPDIR=temp
 OUTDIR=img
 
+rm -r ${TEMPDIR}
+unzip ${PPT_EN} -d ${TEMPDIR}
+
 if [[ -n `which md5sum 2>/dev/null` ]]; then
     if [[ `md5sum ${PPT_EN} | awk '{print $1}'` != ${PPT_EN_MD5} ]]; then
-	echo "The hash value is inconsistent. Extraction may fail..." >&2
+	echo "The source PPTX file has changed. Extraction may fail..." >&2
     fi
 fi
 
-rm -r ${TEMPDIR}
 mkdir -p ${TEMPDIR} ${OUTDIR}
-unzip ${PPT_EN} -d ${TEMPDIR}
 cp ${TEMPDIR}/ppt/media/image5.png ${OUTDIR}/sakura.png
 cp ${TEMPDIR}/ppt/media/image2.png ${OUTDIR}/ribbon_small.png
 cp ${TEMPDIR}/ppt/media/image3.png ${OUTDIR}/ribbon_big.png
